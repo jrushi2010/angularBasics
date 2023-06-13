a2529827e2e7f208f16def53f01680b1e12ecef4
jrushi2010 <jrushi2010@gmail.com>
2023-06-13 14:45:04 +0530

    component communication ngonChanges

---
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { RoomList } from '../rooms';
+import { ChangeDetectionStrategy } from '@angular/compiler';
+
+@Component({
+  selector: 'app-rooms-list',
+  templateUrl: './rooms-list.component.html',
+  styleUrls: ['./rooms-list.component.css']
+})
+export class RoomsListComponent implements OnChanges{
+
+  //parent to child
+  @Input() rooms: RoomList[] = [];
+
+  @Input() title: string = '';
+
+  //child to parent 
+  @Output() selectedRoom = new EventEmitter<RoomList>();
+
+  constructor(){}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if(changes['title']){
+      this.title = changes['title'].currentValue.toUpperCase();
+    }
+  }
+
+  ngOnint(){}
+
+  selectRoom(room: RoomList){
+    this.selectedRoom.emit(room);
+  }
+
+}
