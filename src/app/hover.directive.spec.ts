2905a00a3f7684436d6135717ff096d3484b6f36
jrushi2010 <jrushi2010@gmail.com>
2023-06-16 17:59:43 +0530

    done some small testing practice

--- src/app/hover.directive.spec.ts
+++ src/app/hover.directive.spec.ts
@@ -1,8 +1,9 @@
 import { HoverDirective } from './hover.directive';
+import { ElementRef, Renderer2 } from '@angular/core';
 
 describe('HoverDirective', () => {
   it('should create an instance', () => {
-    const directive = new HoverDirective();
+    const directive = HoverDirective
     expect(directive).toBeTruthy();
   });
 });
--- src/app/rooms/rooms.component.spec.ts
+++ src/app/rooms/rooms.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RoomsComponent } from './rooms.component';
+import { RoomsService } from '../services/rooms.service';
+import { ConfigService } from '../services/config.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouteConfigToken } from '../services/routeConfig.service';
+import { RouterTestingModule } from "@angular/router/testing";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('RoomsComponent', () => {
   let component: RoomsComponent;
@@ -8,7 +14,12 @@ describe('RoomsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RoomsComponent ]
+      imports: [HttpClientTestingModule,RouterTestingModule,FormsModule,ReactiveFormsModule],
+      declarations: [ RoomsComponent ],
+      providers:[RoomsService,ConfigService,{
+        provide: RouteConfigToken,
+        useValue: {title: 'rooms'}
+      }]
     })
     .compileComponents();
 
@@ -20,4 +31,10 @@ describe('RoomsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should toggle',()=>{
+    component.hideRooms = false;
+    component.toggle();
+    expect(component.hideRooms).toBe(true);
+  })
 });
